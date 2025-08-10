ae0332c606a584efea94165af9b703540dc8d9b6
Jacky Lim <jackylim1301@gmail.com>
2025-08-09 23:59:17 -0400

    update git ignore to neglect test logs

--- mcu/inc/state_machine.h
+++ mcu/inc/state_machine.h
@@ -24,9 +24,18 @@
 #define EVT_OBSTACLE_CLEAR  BIT(3)  // Sensor: obstacle cleared
 #define EVT_LOW_BATTERY     BIT(4)  // Battery: critically low
 #define EVT_DOCK_FOUND      BIT(5)  // IR sensor: dock detected
+
 /************************************
  * TYPEDEFS
  ************************************/
+/* Define state enum */
+typedef enum {
+    STATE_IDLE,
+    STATE_ACTIVE,   // Following Pi's command
+    STATE_FAULT,
+    STATE_AVOIDING,
+    STATE_MAX
+} robot_state_t;
 
 /************************************
  * EXPORTED VARIABLES
@@ -40,4 +49,9 @@ void sm_post(uint32_t event_bits);        /* thread or ISR safe */
 void sm_post_obstacle(void);              /* convenience helpers (optional) */
 void sm_post_obstacle_clear(void);
 void sm_post_low_battery(void);
+
+/* Test API */
+int sm_test_get_state(void);
+void sm_test_init(void);        /* initializes FSM but doesn't start forever loop */
+void sm_test_run_step(void);    /* calls smf_run_state() once */
 #endif // STATE_MACHINE_H
